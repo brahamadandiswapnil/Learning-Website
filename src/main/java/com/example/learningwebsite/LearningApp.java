11661b44bf0ec8eacb00c6ff96158f3ec255902b
brahamadandiswapnil <swapnilbramhadandi@gmail.com>
2025-07-28 23:02:41 +0530

    Update and rename LearningAppApplication.java to LearningApp.java

--- src/main/java/com/example/learningwebsite/LearningApp.java
+++ src/main/java/com/example/learningwebsite/LearningApp.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LearningAppApplication {
+public class LearningApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearningAppApplication.class, args);
